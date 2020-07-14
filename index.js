b4e2c7b003fb9664ef8bc58d0d37f89ea84a2611
scrappy-cyborg-4243 <itchkimel@gmail.com>
2020-07-14 03:51:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,14 @@ function logWhisper(string) {
 }
 
  function sayHiToGrandma(string) {  
-  if (string === string.toLowerCase()) return "I can't hear you!"
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!"
+ }
+  
+  if  (string === string.toUpperCase()) {
+    return "YES INDEED!"
+ }
+  if  (string === "I love you, Grandma.") {
+    return "I love you, too."
+ }
 }
-  if  (string === string.toUpperCase()) return "YES INDEED!";
-  if  (string === "I love you, Grandma.") return "I love you, too.";
