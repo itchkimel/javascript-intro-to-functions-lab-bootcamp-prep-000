bd4bb996f9c9a4eeb4fb8b94e722e4c5f12ce40a
scrappy-cyborg-4243 <itchkimel@gmail.com>
2020-07-14 03:17:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string) {
   console.log (string.toLowerCase())
 }
 
- function sayHiToGrandma(string)
-   if (string === string.toLowerCase()) return 
+ function sayHiToGrandma(string){
+   if (string === string.toLowerCase()) return  
    else if 
    else if 
