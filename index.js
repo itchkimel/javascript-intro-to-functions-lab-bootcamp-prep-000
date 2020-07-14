b524fdc4d70d38b2bb0f3e82de6efe42d904ba0e
scrappy-cyborg-4243 <itchkimel@gmail.com>
2020-07-14 02:46:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,4 +17,4 @@ function logWhisper(string) {
  function sayHiToGrandma(string)
   if string === whisper return "I can't hear you";
   else if string === shout return "YES INDEED";
-  else if string === "I love you, Grandma." return 
+  else if string === "I love you, Grandma." return "I love you, too.";
